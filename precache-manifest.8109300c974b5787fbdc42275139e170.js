5800b9992f7206b8a6611539bb01c6d5b8c5468c
shakao <shakao@microsoft.com>
2020-07-27 09:07:27 -0700

    Updates

---
+++ precache-manifest.8109300c974b5787fbdc42275139e170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aee2532d88a951760526b43a8e55f36",
+    "url": "/arcade-sprite-pack/index.html"
+  },
+  {
+    "revision": "a9b3cf05ada8911fb597",
+    "url": "/arcade-sprite-pack/static/css/main.5ced100f.chunk.css"
+  },
+  {
+    "revision": "6dab5c19f579853427da",
+    "url": "/arcade-sprite-pack/static/js/2.696acd36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/arcade-sprite-pack/static/js/2.696acd36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b3cf05ada8911fb597",
+    "url": "/arcade-sprite-pack/static/js/main.fc8ed8fd.chunk.js"
+  },
+  {
+    "revision": "8273726a85d79466ed01",
+    "url": "/arcade-sprite-pack/static/js/runtime-main.d3428316.js"
+  }
+]);
