20ca76172ab2cdd579e0a587d7564712f1942819
shakao <shakao@microsoft.com>
2020-07-27 09:00:23 -0700

    Create react app, add iframe with asset editor

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <iframe id="editor" src="http://arcade.makecode.com/beta--asseteditor" />
+    </div>
+  );
+}
+
+export default App;
