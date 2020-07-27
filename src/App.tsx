6b5606e4775b509e405df1cf119c84b5857c3f19
Richard Knoll <riknoll@microsoft.com>
2020-07-27 16:34:52 -0700

    revert accidental add

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import AssetList from './components/AssetList';
 import './styles/App.css';
-import { TestWidget } from './components/TestWidget';
 
 function App() {
   return (
     <div className="app">
       <iframe id="editor" title="MakeCode Arcade sprite editor" src="https://arcade.makecode.com/beta--asseteditor" />
       <AssetList />
-      <TestWidget></TestWidget>
     </div>
   );
 }
