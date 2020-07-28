d034788d8411731cfe3b891751c38609ed2fa16a
shakao <34112083+shakao@users.noreply.github.com>
2020-07-28 08:22:43 -0700

    Hook up iframe messaging, save jres in asset (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import AssetList from './components/AssetList';
+import { AssetInfo } from './components/Asset';
 import './styles/App.css';
 
-function App() {
-  return (
-    <div className="app">
-      <iframe id="editor" title="MakeCode Arcade sprite editor" src="https://arcade.makecode.com/beta--asseteditor" />
-      <AssetList />
-    </div>
-  );
+class App extends React.Component {
+    private iframe!: HTMLIFrameElement;
+
+    constructor(props: {}) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.iframe = document.getElementById("editor") as HTMLIFrameElement;
+    }
+
+    postMessage = (data: any) => {
+        if (this.iframe && this.iframe.contentWindow) {
+            data["_fromVscode"] = true; // fake _fromVscode for now
+            this.iframe.contentWindow.postMessage(data, "*");
+        }
+    }
+
+    loadJres = (asset: AssetInfo) => {
+        this.postMessage({ type: "initialize", message: asset.jres })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <iframe id="editor" title="MakeCode Arcade sprite editor" src="https://arcade.makecode.com/beta--asseteditor" />
+                <AssetList onItemSelect={this.loadJres} />
+            </div>
+        );
+    }
 }
 
 export default App;
