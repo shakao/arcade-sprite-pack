453e12665e1602881027f284d7b48ce7330fe29a
Richard Knoll <riknoll@users.noreply.github.com>
2020-07-27 17:01:48 -0700

    Adding code for grabbing images from a published script (#3)
    * Adding code for grabbing images from a published script
    
    * revert accidental add

--- src/images.ts
+++ src/images.ts
@@ -1,4 +1,15 @@
 
+export interface JRESImage {
+    data: string; // Base64 encoded string of f4 encoded bytes
+    previewURI: string; // PNG data uri
+    width: number;
+    height: number;
+
+    qualifiedName?: string;
+    tilemapTile?: boolean;
+    sourceFile?: string;
+}
+
 export interface ImageInfo {
     width: number;
     height: number;
@@ -21,6 +32,40 @@ export async function getImageFromURIAsync(uri: string): Promise<ImageInfo | nul
         height: data.height,
         pixels: data.data
     };
+}
+
+async function loadImageAsync(uri: string): Promise<HTMLImageElement> {
+    return new Promise<HTMLImageElement>(resolve => {
+        const el = document.createElement("img");
+        el.src = uri
+        el.onload = () => {
+            resolve(el)
+        };
+    })
+}
+
+export function getJRESImageFromDataString(data: string, palette: string[], qname?: string, tilemapTile?: boolean, sourceFile?: string): JRESImage {
+    const bitmap = jresDataToBitmap(data);
+
+    return getJRESImage(bitmap, palette, data, qname, tilemapTile, sourceFile)
+}
+
+export function getJRESImageFromImageLiteral(literal: string, palette: string[], sourceFile?: string) {
+    const bitmap = imageLiteralToBitmap(literal);
+    return getJRESImage(bitmap, palette, undefined, undefined, undefined, sourceFile)
+}
+
+
+function getJRESImage(bitmap: Bitmap, palette: string[], data?: string, qname?: string, tilemapTile?: boolean, sourceFile?: string): JRESImage {
+    return {
+        width: bitmap.width,
+        height: bitmap.height,
+        data: data || base64EncodeBitmap(bitmap),
+        previewURI: bitmapToImageURI(bitmap, palette),
+        qualifiedName: qname,
+        tilemapTile,
+        sourceFile
+    }
 }
 
 /**
@@ -58,16 +103,6 @@ function colorsToNumbers(colors: string[]): number[][] {
         res.push([_r(color), _g(color), _b(color)]);
     }
     return res;
-}
-
-async function loadImageAsync(uri: string): Promise<HTMLImageElement> {
-    return new Promise<HTMLImageElement>(resolve => {
-        const el = document.createElement("img");
-        el.src = uri
-        el.onload = () => {
-            resolve(el)
-        };
-    })
 }
 
 // use geometric distance on colors
@@ -140,6 +175,11 @@ function imgEncodeImgCore(w: number, h: number, getPix: (x: number, y: number) =
     return res;
 }
 
+function base64EncodeBitmap(bitmap: Bitmap) {
+    const hex = f4EncodeImgCore(bitmap.width, bitmap.height, 4, (x, y) => bitmap.get(x, y));
+    return btoa(uint8ArrayToString(hexToUint8Array(hex)))
+}
+
 function _r(color: number) { return (color >> 16) & 0xff }
 function _g(color: number) { return (color >> 8) & 0xff }
 function _b(color: number) { return color & 0xff }
@@ -154,4 +194,217 @@ function parseColorString(color: string) {
         }
     }
     return 0;
+}
+
+/**
+ * 16-color sprite
+ */
+export class Bitmap {
+    protected buf: Uint8ClampedArray;
+
+    constructor(public width: number, public height: number, public x0 = 0, public y0 = 0, buf?: Uint8ClampedArray) {
+        this.buf = buf || new Uint8ClampedArray(this.dataLength());
+    }
+
+    set(col: number, row: number, value: number) {
+        if (col < this.width && row < this.height && col >= 0 && row >= 0) {
+            const index = this.coordToIndex(col, row);
+            this.setCore(index, value);
+        }
+    }
+
+    get(col: number, row: number) {
+        if (col < this.width && row < this.height && col >= 0 && row >= 0) {
+            const index = this.coordToIndex(col, row);
+            return this.getCore(index);
+        }
+        return 0;
+    }
+
+    protected coordToIndex(col: number, row: number) {
+        return col + row * this.width;
+    }
+
+    protected getCore(index: number) {
+        const cell = Math.floor(index / 2);
+        if (index % 2 === 0) {
+            return this.buf[cell] & 0xf;
+        }
+        else {
+            return (this.buf[cell] & 0xf0) >> 4;
+        }
+    }
+
+    protected setCore(index: number, value: number) {
+        const cell = Math.floor(index / 2);
+        if (index % 2 === 0) {
+            this.buf[cell] = (this.buf[cell] & 0xf0) | (value & 0xf);
+        }
+        else {
+            this.buf[cell] = (this.buf[cell] & 0x0f) | ((value & 0xf) << 4);
+        }
+    }
+
+    protected dataLength() {
+        return Math.ceil(this.width * this.height / 2);
+    }
+}
+
+
+function jresDataToBitmap(jresURL: string) {
+    let data = atob(jresURL.slice(jresURL.indexOf(",") + 1))
+    let magic = data.charCodeAt(0);
+    let w = data.charCodeAt(1);
+    let h = data.charCodeAt(2);
+
+    if (magic === 0x87) {
+        magic = 0xe0 | data.charCodeAt(1);
+        w = data.charCodeAt(2) | (data.charCodeAt(3) << 8);
+        h = data.charCodeAt(4) | (data.charCodeAt(5) << 8);
+        data = data.slice(4);
+    }
+
+    const out = new Bitmap(w, h);
+
+    let index = 4
+    if (magic === 0xe1) {
+        // Monochrome
+        let mask = 0x01
+        let v = data.charCodeAt(index++)
+        for (let x = 0; x < w; ++x) {
+            for (let y = 0; y < h; ++y) {
+                out.set(x, y, (v & mask) ? 1 : 0);
+                mask <<= 1
+                if (mask == 0x100) {
+                    mask = 0x01
+                    v = data.charCodeAt(index++)
+                }
+            }
+        }
+    }
+    else {
+        // Color
+        for (let x = 0; x < w; x++) {
+            for (let y = 0; y < h; y += 2) {
+                let v = data.charCodeAt(index++)
+                out.set(x, y, v & 0xf);
+                if (y != h - 1) {
+                    out.set(x, y + 1, (v >> 4) & 0xf);
+                }
+            }
+            while (index & 3) index++
+        }
+    }
+
+    return out;
+}
+
+function imageLiteralToBitmap(text: string, defaultPattern?: string): Bitmap {
+    // Strip the tagged template string business and the whitespace. We don't have to exhaustively
+    // replace encoded characters because the compiler will catch any disallowed characters and throw
+    // an error before the decompilation happens. 96 is backtick and 9 is tab
+    text = text.replace(/[ `]|(?:&#96;)|(?:&#9;)|(?:img)/g, "").trim();
+    text = text.replace(/^["`\(\)]*/, '').replace(/["`\(\)]*$/, '');
+    text = text.replace(/&#10;/g, "\n");
+
+    if (!text && defaultPattern)
+        text = defaultPattern;
+
+    const rows = text.split("\n");
+
+    // We support "ragged" sprites so not all rows will be the same length
+    const sprite: number[][] = [];
+    let spriteWidth = 0;
+
+    for (let r = 0; r < rows.length; r++) {
+        const row = rows[r];
+        const rowValues: number[] = [];
+        for (let c = 0; c < row.length; c++) {
+            // This list comes from libs/screen/targetOverrides.ts in pxt-arcade
+            // Technically, this could change per target.
+            switch (row[c]) {
+                case "0": case ".": rowValues.push(0); break;
+                case "1": case "#": rowValues.push(1); break;
+                case "2": case "T": rowValues.push(2); break;
+                case "3": case "t": rowValues.push(3); break;
+                case "4": case "N": rowValues.push(4); break;
+                case "5": case "n": rowValues.push(5); break;
+                case "6": case "G": rowValues.push(6); break;
+                case "7": case "g": rowValues.push(7); break;
+                case "8": rowValues.push(8); break;
+                case "9": rowValues.push(9); break;
+                case "a": case "A": case "R": rowValues.push(10); break;
+                case "b": case "B": case "P": rowValues.push(11); break;
+                case "c": case "C": case "p": rowValues.push(12); break;
+                case "d": case "D": case "O": rowValues.push(13); break;
+                case "e": case "E": case "Y": rowValues.push(14); break;
+                case "f": case "F": case "W": rowValues.push(15); break;
+            }
+        }
+
+        if (rowValues.length) {
+            sprite.push(rowValues);
+            spriteWidth = Math.max(spriteWidth, rowValues.length);
+        }
+    }
+
+    const spriteHeight = sprite.length;
+
+    const result = new Bitmap(spriteWidth, spriteHeight)
+
+    for (let r = 0; r < spriteHeight; r++) {
+        const row = sprite[r];
+        for (let c = 0; c < spriteWidth; c++) {
+            if (c < row.length) {
+                result.set(c, r, row[c]);
+            }
+            else {
+                result.set(c, r, 0);
+            }
+        }
+    }
+
+    return result;
+}
+
+/**
+ * Converts a bitmap into a square image suitable for display
+ */
+function bitmapToImageURI(frame: Bitmap, colors: string[]) {
+    const canvas = document.createElement("canvas");
+    canvas.width = frame.width;
+    canvas.height = frame.height;
+
+    let context = canvas.getContext("2d");
+
+    if (context) {
+        for (let c = 0; c < frame.width; c++) {
+            for (let r = 0; r < frame.height; r++) {
+                const color = frame.get(c, r);
+
+                if (color) {
+                    context.fillStyle = colors[color];
+                    context.fillRect(c, r, 1, 1);
+                }
+            }
+        }
+    }
+
+
+    return canvas.toDataURL();
+}
+
+function hexToUint8Array(hex: string) {
+    let r = new Uint8ClampedArray(hex.length >> 1);
+    for (let i = 0; i < hex.length; i += 2)
+        r[i >> 1] = parseInt(hex.slice(i, i + 2), 16)
+    return r
+}
+
+function uint8ArrayToString(input: ArrayLike<number>) {
+    let len = input.length;
+    let res = ""
+    for (let i = 0; i < len; ++i)
+        res += String.fromCharCode(input[i]);
+    return res;
 }
