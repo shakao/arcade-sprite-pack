e3dd0cf4aff0ef3e456d31ac31937ced9b778e92
Richard Knoll <riknoll@microsoft.com>
2022-08-16 10:15:33 -0700

    Updates

---
+++ precache-manifest.4c4c8ad5a99455673839ddcc7c3524b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4796bfb19e2f2f4c74b94fab2bf8c1",
+    "url": "/arcade-sprite-pack/index.html"
+  },
+  {
+    "revision": "27fbb759013d2e5aac50",
+    "url": "/arcade-sprite-pack/static/css/main.eef947b3.chunk.css"
+  },
+  {
+    "revision": "a8fbe33b9f8f35a6b113",
+    "url": "/arcade-sprite-pack/static/js/2.09115f4a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/arcade-sprite-pack/static/js/2.09115f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27fbb759013d2e5aac50",
+    "url": "/arcade-sprite-pack/static/js/main.723889e4.chunk.js"
+  },
+  {
+    "revision": "8273726a85d79466ed01",
+    "url": "/arcade-sprite-pack/static/js/runtime-main.d3428316.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/arcade-sprite-pack/precache-manifest.86d7ffce03ccf95b86d04036e49c3a89.js"
+  "/arcade-sprite-pack/precache-manifest.4c4c8ad5a99455673839ddcc7c3524b7.js"
 );
 
 self.addEventListener('message', (event) => {
